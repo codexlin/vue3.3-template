36a3cf29723d8256d1ae2d0fe1bdb26436253925
LinRenJie <xoxosos666@gmail.com>
2023-10-09 22:25:21 +0800

    :sparkles: 模版项目搭建

---
+++ src/components/setting/SettingWrapper.vue
@@ -0,0 +1,43 @@
+<template>
+  <a-button
+    @click="showDrawer"
+    class="setting-fixed"
+    type="primary"
+    shape="circle"
+    :icon="h(SettingOutlined)"
+  />
+  <a-drawer
+    v-model:open="open"
+    class="custom-class"
+    root-class-name="root-class-name"
+    :root-style="{ color: 'blue' }"
+    style="color: red"
+    title="Basic Drawer"
+    placement="right"
+    @after-open-change="afterOpenChange"
+  >
+    <SettingView />
+  </a-drawer>
+</template>
+<script lang="ts" setup>
+import { SettingOutlined } from '@ant-design/icons-vue'
+import { h, ref } from 'vue'
+import SettingView from './SettingView.vue'
+const open = ref<boolean>(false)
+
+const afterOpenChange = (bool: boolean) => {
+  console.log('open', bool)
+}
+
+const showDrawer = () => {
+  open.value = true
+}
+</script>
+<style lang="scss" scoped>
+.setting-fixed {
+  position: fixed;
+  z-index: 2;
+  right: 0;
+  top: 50%;
+}
+</style>
